7f274ce0a136bf2eb62ff222fc0f8a3ef8d6828a
temnyy <serdar.shyl@gmail.com>
2026-09-08 10:59:41 +0300

    analytics yüklendi

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -3,10 +3,12 @@ import Selection from "@/components/Selection";
 import Expertise from "@/components/Expertise";
 import Clientele from "@/components/Clientele";
 import Contact from "@/components/Contact";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <main>
+      <Analytics />
       <IntroStage />
       <Selection />
       <Expertise />
